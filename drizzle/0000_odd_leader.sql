254c8039451a99eeace31c1c37c86f6cca21e3c1
steven <stevensan0606@gmail.com>
2024-10-14 03:29:34 -0400

    drizzle in gitignore

--- drizzle/0000_odd_leader.sql
+++
@@ -1,8 +0,0 @@
--- Current sql file was generated after introspecting the database
--- If you want to run this migration please uncomment this code before executing migrations
-/*
-CREATE TABLE `users` (
-	`id` integer PRIMARY KEY
-);
-
-*/
