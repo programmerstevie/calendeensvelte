ac3f8520e87c0484b2cf7d0a69f1aa0128d60099
steven <stevensan0606@gmail.com>
2024-10-19 21:40:06 -0400

    added maghrib notif

---
+++ src/lib/components/FiveStars.svelte
@@ -0,0 +1,33 @@
+<div class="rating rating-sm space-x-1">
+  <input
+    type="radio"
+    name="rating-1"
+    class="mask mask-star-2 hover:cursor-default bg-secondary"
+    disabled
+  />
+  <input
+    type="radio"
+    name="rating-1"
+    class="mask mask-star-2 hover:cursor-default bg-secondary"
+    disabled
+  />
+  <input
+    type="radio"
+    name="rating-1"
+    class="mask mask-star-2 hover:cursor-default bg-secondary"
+    disabled
+  />
+  <input
+    type="radio"
+    name="rating-1"
+    class="mask mask-star-2 hover:cursor-default bg-secondary"
+    disabled
+  />
+  <input
+    type="radio"
+    name="rating-1"
+    class="mask mask-star-2 hover:cursor-default bg-secondary"
+    disabled
+    checked="checked"
+  />
+</div>
--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,10 +1,22 @@
 <script>
   import { twMerge } from "tailwind-merge";
   import Container from "./Container.svelte";
+  import FiveStars from "./FiveStars.svelte";
+  import IoMdGift from "svelte-icons/io/IoMdGift.svelte";
 
-  const sale = false;
-  const num_customers = 1000;
+  const sale_num = 1000;
+  const num_customers = 973;
+  $: sale = sale_num > num_customers;
 </script>
+
+<div class="fixed top-3 h-16 w-[100vw] flex justify-center">
+  <img
+    src="athan_notification_1.png"
+    alt="annoying notification"
+    class="h-full animate-slideDown opacity-0"
+    style={"animation-delay: 3s"}
+  />
+</div>
 
 <Container class="lg:mt-20 outer-container text-center lg:text-left">
   <div class="lg:flex">
@@ -18,7 +30,7 @@
         <span class="dafont uppercase text-base-200 relative ml-2 lg:ml-0 lg:block lg:mt-2">
           <div class="inline-block animate-bounce-realistics text-nowrap">your Khushu'</div>
           <span
-            class="absolute h-[1.1em] w-[8em] -left-4 sm:left-[-10px] lg:-left-4 top-[8px] sm:top-[5px] lg:top-0 bg-base-content rotate-[-1deg] -z-10"
+            class="absolute h-[1.1em] w-[8em] -left-4 sm:left-[-10px] lg:-left-4 top-[5px] lg:top-0 bg-base-content rotate-[-1deg] -z-10"
           ></span>
         </span>
       </h1>
@@ -26,20 +38,26 @@
         The calendar app with all you need to schedule your life around your prayer times.
       </p>
       <button
-        class="btn btn-primary lg:mt-16 mt-10 btn-wide h-14 text-xl font-bold flex items-center mx-auto lg:mx-0"
+        class="btn btn-primary lg:mt-12 mt-10 btn-wide text-md font-bold flex items-center mx-auto lg:mx-0"
       >
         Get Calendeen
       </button>
-      {#if sale}
-        <div class="mt-10 text-xl text-secondary-content">
-          Limited Time Sale! 20% off for the first 100 customers (13 left)
-        </div>
-      {/if}
-      <section class="flex w-fit mx-auto lg:mx-0 mt-16 bg-red-500s">
+      <div class={twMerge("text-sm mt-2 font-bold text-base-content", sale ? "" : "invisible")}>
+        <span class="text-green-500">
+          <span class="inline-block w-6 align-middle mr-1 animate-pulse">
+            <IoMdGift />
+          </span>
+          $20 off</span
+        >
+        for the first {sale_num} customers ({sale_num - num_customers} left)
+      </div>
+      <section
+        class="flex w-fit mx-auto lg:mx-0 flex-col items-center xl:flex-row mt-14 bg-red-500s"
+      >
         <div class="avatar-group -space-x-6">
-          {#each [1, 1, 1, 1, 1] as _}
+          {#each Array(5) as _}
             <div class="avatar">
-              <div class="w-12">
+              <div class="w-11">
                 <img
                   class="bg-neutral"
                   src="https://cdni.iconscout.com/illustration/premium/thumb/muslim-man-illustration-download-in-svg-png-gif-file-formats--arabic-male-islamic-faceless-character-of-pack-people-illustrations-10332654.png?f=webp"
@@ -49,40 +67,8 @@
             </div>
           {/each}
         </div>
-        <div class="ml-7">
-          <div class="rating rating-xs">
-            <input
-              type="radio"
-              name="rating-1"
-              class="mask mask-star-2 hover:cursor-default bg-secondary"
-              disabled
-            />
-            <input
-              type="radio"
-              name="rating-1"
-              class="mask mask-star-2 hover:cursor-default bg-secondary"
-              disabled
-            />
-            <input
-              type="radio"
-              name="rating-1"
-              class="mask mask-star-2 hover:cursor-default bg-secondary"
-              disabled
-            />
-            <input
-              type="radio"
-              name="rating-1"
-              class="mask mask-star-2 hover:cursor-default bg-secondary"
-              disabled
-            />
-            <input
-              type="radio"
-              name="rating-1"
-              class="mask mask-star-2 hover:cursor-default bg-secondary"
-              disabled
-              checked="checked"
-            />
-          </div>
+        <div class="ml-7 text-center xl:text-left">
+          <FiveStars />
           <div class="font-bold">
             <span class="font-bold bg-primary text-primary-content px-1">{num_customers}</span> muslims
             plan with Calendeen.
